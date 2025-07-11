970afba65b801bd071bab8547841d25696b58d15
dpiperato <dpiperato@allinsolutions.com>
2025-07-11 18:30:05 -0400

    Update layout.jsx
    replaced invoca script with CTM script

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -29,16 +29,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles />
+
       <Helmet>
-      <script>
-      {`
-      (function(i,n,v,o,c,a) { i.InvocaTagId = o; var s = n.createElement('script'); s.type = 'text/javascript';
-      s.async = true; s.src = ('https:' === n.location.protocol ? 'https://' : 'http://' ) + v;
-      var fs = n.getElementsByTagName('script')[0]; fs.parentNode.insertBefore(s, fs);
-  })(window, document, 'solutions.invocacdn.com/js/invoca-latest.min.js', '1972/1573260581');
-      `}
-      </script>      
-      </Helmet>
+  <script async src="//322114.tctm.xyz/t.js"></script>
+</Helmet>
+
+
       <div ref={node} className={`d-block d-xl-none`}>
         <BurgerButton setOpen={setMobileNavOpen} open={mobileNavOpen} />
         <MobileMenu open={mobileNavOpen} />
